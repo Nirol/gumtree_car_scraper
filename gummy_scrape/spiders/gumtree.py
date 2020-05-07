c55a84396a681315b68f9ad3626adc69c7adacfd
JohnKrige <krigejohn@gmail.com>
2020-05-07 11:09:53 +0200

    Imporved with new features scraped

--- gummy_scrape/spiders/gumtree.py
+++ gummy_scrape/spiders/gumtree.py
@@ -12,29 +12,42 @@ def description_info(response,value):
     return response.xpath('//div[@class="description-content"]/b[text()="'+ value +'"]/following-sibling::text()').get()  
 
 
-
 class GumtreeSpider(scrapy.Spider):
     name = 'gumtree'
     # allowed_domains = ['https://gumtree.co.za/']
     start_urls = ['https://gumtree.co.za/s-cars-bakkies/v1c9077p1']
 
     def parse(self, response):
-        car_links = response.xpath('//*[@class="related-ad-title"]/@href').extract() 
-        for car in car_links:
-            absolute_url = 'https://www.gumtree.co.za' + car
-            yield scrapy.Request(absolute_url,callback= self.parse_car)
+        # Province links - from the main page in "Cars and Bakkies"
+        for province in response.xpath('//*[@class="finalsub inventoryView more"]/li/span[@class="text"]/a/@href').extract()[:9]:
+            absolute_province_url = 'https://www.gumtree.co.za' + province
+            yield scrapy.Request(absolute_province_url)
 
-        next_page_url =  response.xpath('//*[@class=" icon-pagination-right"]/@href').get() 
+            # list of location links per province 
+            for location in response.xpath('//div[@class="foldableList"]/ul[contains(@class,"finalsub")]/li/span/a/@href').extract()[1:]:
+                absolute_location_url = 'https://www.gumtree.co.za' + location
+                yield scrapy.Request(absolute_location_url)
 
-        if next_page_url:
-            absolute_next_page_url = 'https://www.gumtree.co.za' + next_page_url
-            yield scrapy.Request(absolute_next_page_url)
+                # Car add links per page -  for a specific location
+                car_links = response.xpath('//*[@class="related-ad-title"]/@href').extract() 
+                for car in car_links:
+                    absolute_car_url = 'https://www.gumtree.co.za' + car
+                    yield scrapy.Request(absolute_car_url,callback= self.parse_car)
+
+                next_page_url =  response.xpath('//*[@class=" icon-pagination-right"]/@href').get() 
+
+                if next_page_url:
+                    absolute_next_page_url = 'https://www.gumtree.co.za' + next_page_url
+                    yield scrapy.Request(absolute_next_page_url)
+
 
     def parse_car(self,response):
+        add_url = response.url
         title = response.xpath('///h1/text()').get()
         price = response.xpath('//*[@class="ad-price"]/text()').get()
         #Clean price text: 
         price =  price.splitlines()[1].split('R')[1]
+        province = response.xpath('//h2/span/span/a/span/text()').extract()[0]
         suburb = response.xpath('//*[@class="location"]/a[1]/text()').extract()
         city = response.xpath('//*[@class="location"]/a[2]/text()').extract()
 
@@ -59,14 +72,13 @@ class GumtreeSpider(scrapy.Spider):
         tank_capacity  = description_info(response,'Fuel Tank Capacity')
         service_intervals = description_info(response,'Service Intervals')
 
-
-
         yield{
             'date':datetime.datetime.now(),
             'title':title,
             'price':price,
             'suburb':suburb,
             'city':city,
+            'province':province,
             'make':make,
             'model':model,
             'for_sale_by':for_sale_by,
@@ -82,6 +94,7 @@ class GumtreeSpider(scrapy.Spider):
             'seats':seats,
             'tank_capacity':tank_capacity,
             'service_intervals':service_intervals,
+            'url': add_url
         }
 
 
